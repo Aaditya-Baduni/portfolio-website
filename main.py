f1eaac6c47dce974fe12bf9bbb43a247e18dbf45
Aaditya-Baduni <aadityalaformidable@gmail.com>
2023-12-16 10:41:16 +0530

    Added app names, descriptions, images in 2 separate rows

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -15,3 +16,19 @@ with col2:
     st.info(content)
 
 st.write("Below you can find some of the apps that I have built using Python. Feel free to contact me (Actually, don't, it's just a formality to say this)!")
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row["image"]}")
+        st.write(f"[Source code]({row["url"]})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row["image"]}")
+        st.write(f"[Source code]({row["url"]})")
